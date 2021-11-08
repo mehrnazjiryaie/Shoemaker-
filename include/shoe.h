434dffb3670131889c6c176cf14fb7e233d80625
Mehrnaz <mehrnazjiryaie@gmail.com>
2021-11-08 17:43:53 +0330

    Commit 1.2

---
+++ include/shoe.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class SportShoe
+{
+private:
+    int shoelase; //meter
+    double cloth; //square meters
+    int size; //size of shoe
+    std::string color; //color of shoe
+    
+public:
+    SportShoe(/* args */);
+    ~SportShoe();
+};
+
+SportShoe::SportShoe(/* args */)
+{
+}
+
+SportShoe::~SportShoe()
+{
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    cout << "Hello project phase 1!!!\n";
+    
     return 0;
 }
